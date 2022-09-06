3517a68ddb83c3301f4380f7453db605e89ff588
ltr <litianr@mail2.sysu.edu.cn>
2022-09-06 10:19:00 +0800

    -done: move the enumerations of Multiplier/Adder types to OperatorType in top package

--- src/main/scala/arithmetic/package.scala
+++ src/main/scala/arithmetic/package.scala
@@ -1,18 +1,4 @@
 package org.datenlord
 
-package object arithmetic {
-
-  object MultiplierMode extends Enumeration {
-    val FULL, LSB, MSB, SQUARE, FULL34, COMPLEX = Value
-    type MultiplierMode = Value
-  }
-
-  object AdderMode extends Enumeration {
-    val BINARY, TERNARY = Value
-    type AdderMode = Value
-  }
-
-
-
-}
+package object arithmetic {}
 
