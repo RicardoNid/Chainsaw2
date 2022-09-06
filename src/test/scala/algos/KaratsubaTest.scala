db2d3afda60dea6a878ecc1df7cf3cc90b708dc6
ltr <litianr@mail2.sysu.edu.cn>
2022-09-06 10:14:18 +0800

    - tiny modification

--- src/test/scala/algos/KaratsubaTest.scala
+++ src/test/scala/algos/KaratsubaTest.scala
@@ -3,7 +3,6 @@ package algos
 
 import org.scalatest.flatspec.AnyFlatSpec
 import spinal.core._
-import arithmetic.MultplierMode._
 
 import spinal.core
 
@@ -20,33 +19,26 @@ class KaratsubaTest extends AnyFlatSpec {
   val ys = data.takeRight(testCount)
 
   "Karatsuba Algo" should "work for all situation" in {
-    val modes = Seq(FULL, LSB, SQUARE)
+    val modes = Seq(FullMultiplier, LsbMultiplier, SquareMultiplier)
     modes.foreach { mode =>
       val kara = Karatsuba(width, mode, baseWidth)
-      xs.zip(ys).foreach { case (x, y) => if (mode == SQUARE) kara.mult(x, x) else kara.mult(x, y) }
+      xs.zip(ys).foreach { case (x, y) => if (mode == SquareMultiplier) kara.mult(x, x) else kara.mult(x, y) }
     }
   }
 
-  "3-stage Karatsuba Algo" should "work for all situation" in {
-    val modes = Seq(FULL, LSB, SQUARE)
-    modes.foreach { mode =>
-      val kara = Karatsuba(width, mode, baseWidth)
-      xs.zip(ys).foreach { case (x, y) => if (mode == SQUARE) kara.multImproved(x, x) else kara.multImproved(x, y) }
-    }
-  }
 
   it should "show the cost estimated" in {
-    val modes = Seq(FULL, LSB, SQUARE)
+    val modes = Seq(FullMultiplier, LsbMultiplier, SquareMultiplier)
     modes.foreach { mode =>
       val kara = Karatsuba(width, mode, baseWidth)
       val x = xs.head
       val y = ys.head
-      if (mode == SQUARE) kara.multImproved(x, x, verbose = true) else kara.multImproved(x, y, verbose = true)
+      if (mode == SquareMultiplier) kara.multImproved(x, x, verbose = true) else kara.multImproved(x, y, verbose = true)
     }
   }
 
   "Karatsuba96" should "work" in {
-    val kara = Karatsuba(width, FULL, baseWidth)
+    val kara = Karatsuba(width, FullMultiplier, baseWidth)
     val a = Random.nextBigInt(96)
     val b = Random.nextBigInt(96)
     val ret = kara.karatsuba96(a,b)
@@ -54,7 +46,7 @@ class KaratsubaTest extends AnyFlatSpec {
   }
 
   "Karatsuba377" should "show the cost estimated" in {
-    val kara = Karatsuba(width, FULL, baseWidth)
+    val kara = Karatsuba(width, FullMultiplier, baseWidth)
     kara.karatsuba377(xs.head, ys.head)
   }
 
