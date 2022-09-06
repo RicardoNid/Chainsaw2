601307d4c0f6ded9f0b7ed4a172040f7750331de
ltr <litianr@mail2.sysu.edu.cn>
2022-09-06 09:34:14 +0800

    -done: replace the positive + negative compressors by a mixed compressor -todo: this rewriting method should apply to all adder trees within the DAG, rather than the ones at the end of a DAG

--- src/main/scala/dfg/RewriteAdditionTree.scala
+++ src/main/scala/dfg/RewriteAdditionTree.scala
@@ -1,15 +1,20 @@
 package org.datenlord
 package dfg
 
-import org.datenlord.arithmetic.BitHeapCompressorConfig
+import arithmetic.{ArithInfo, BitHeapCompressorUseInversionConfig}
+
 import spinal.core._
 
 import scala.collection.mutable.ArrayBuffer
 
-object RewritePostAdditionTree {
+object RewriteAdditionTree {
 
-  /** in-place rewrite method that extract the post-addition tree and convert it into a compressor(CSA) tree
+  // FIXME: SHOULD WORK WHEN NO EXTRACTION CAN BE MADE
+  // FIXME: SHOULD WORK WHEN FULL EXTRACTION CAN BE MADE
+
+  /** in-place rewrite method that extract the addition tree and convert it into a compressor(CSA) tree
    *
+   * a addition tree is a multi-operand addition
    */
   def apply(ringDag: RingDag): Unit = {
 
@@ -23,8 +28,7 @@ object RewritePostAdditionTree {
     type E = DagEdge
     type Port = DagPort[UInt]
 
-    import OpType._
-    val postTypes = Seq(ADDC, ADD, SUBC, SUB, SHIFT, RESIZE) // types which are part of post-addition tree
+    val postTypes = Seq(BinaryAdder, BinarySubtractor, Shift, Resize) // types which are part of post-addition tree
 
     def isInTree(v: V) = postTypes.contains(v.opType)
 
@@ -63,64 +67,24 @@ object RewritePostAdditionTree {
       val drivingEdges = drivingVertices.flatMap { case (v, info) =>
         v.opType match {
           // keep
-          case ADD => v.incomingEdges.map(e => (e, info))
-          case ADDC => v.incomingEdges.map(e => (e, info))
-          case RESIZE => v.incomingEdges.map(e => (e, info))
+          case BinaryAdder => v.incomingEdges.map(e => (e, info))
+          case Resize => v.incomingEdges.map(e => (e, info))
           // inverse
-          case SUBC =>
-            val Seq(e0, e1) = v.incomingEdges
-            Seq((e0, info), (e1, -info))
-          case SUB =>
+          case BinarySubtractor =>
             val Seq(e0, e1) = v.incomingEdges
             Seq((e0, info), (e1, -info))
           // shift
-          case SHIFT => v.incomingEdges.map(e => (e, info << extractShift(v.asInstanceOf[RingVertex])))
+          case Shift => v.incomingEdges.map(e => (e, info << extractShift(v.asInstanceOf[RingVertex])))
         }
       }
       drivingEdges.foreach { case (edge, info) => postEdges += edge }
 
       currents = drivingEdges // update currents for next step
     }
-    val allInfos = operands.map { case (e, info) => ArithInfo(e.sourcePort.asInstanceOf[RingPort].width, info.shift, info.sign) }
+    val allInfos = operands.map { case (e, info) => ArithInfo(e.sourcePort.asInstanceOf[RingPort].width, info.weight, info.sign) }
+    val allOps = operands.map(_._1.sourcePort.asInstanceOf[RingPort])
 
-    //    /** --------
-    //     * print infos
-    //     * -------- */
-    val posOps = operands.filter(_._2.sign == true)
-    val negOps = operands.filter(_._2.sign == false)
-
-    val posInfos = allInfos.filter(_.sign == true)
-    val negInfos = allInfos.filter(_.sign == false)
-
-    logger.info(s"number of operands: ${allInfos.length}")
-    logger.info(s"number of different shifts: ${allInfos.map(_.shift).distinct.length}")
-    allInfos.groupBy(_.shift).toSeq.sortBy(_._1).foreach { case (shift, infos) =>
-      //      logger.info(s"operand at shift $shift: ${infos.map(_.width).mkString(" ")}")
-    }
-    logger.info(s"total bits for reduction ${allInfos.map(_.width).sum}")
-    logger.info(s"positive bits for reduction ${posInfos.map(_.width).sum}")
-    logger.info(s"negative bits for reduction ${negInfos.map(_.width).sum}")
-
-    /** --------
-     * construct a new tree(s)
-     * -------- */
-    val compress0 = CompressorVertex("COMPRESS+", posInfos)
-    ringDag.addVertexWithDrivers(compress0, posOps.map(_._1.sourcePort): _*)
-
-    val ret = if (negInfos.isEmpty) {
-      val wo0 = compress0.widthsOut.head
-      val cpa0 = compress0.out(0).resize(wo0) +:+^ compress0.out(1).resize(wo0)
-      cpa0
-    } else {
-      val wo0 = compress0.widthsOut.head
-      val cpa0 = compress0.out(0).resize(wo0) +:+^ compress0.out(1).resize(wo0)
-
-      val compress1 = CompressorVertex("COMPRESS-", negInfos.map(info => ArithInfo(info.width, info.shift, true)))
-      ringDag.addVertexWithDrivers(compress1, negOps.map(_._1.sourcePort): _*)
-      val wo1 = compress1.widthsOut.head
-      val cpa1 = compress1.out(0).resize(wo1) +:+^ compress1.out(1).resize(wo1) // pass
-      cpa0 -:- cpa1
-    }
+    val ret = BitHeapCompressorUseInversionConfig(allInfos).asRingOp(allOps).head
 
     val resized = ret.resize(root.widthsIn.head)
     logger.info(s"output width ${root.widthsIn.head}")
