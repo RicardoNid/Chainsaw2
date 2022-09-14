230f16bf99442c96be73e47d6d817cdacf2f4595
lqx <liqx36@mail2.sysu.edu.cn>
2022-09-14 09:00:02 +0800

    1) Change the way the frequency is extracted 2) Fix regular expression of the method of UltraScale's LUT extraction

--- src/main/scala/xilinx/package.scala
+++ src/main/scala/xilinx/package.scala
@@ -25,7 +25,8 @@ package object xilinx {
     resetKind = ASYNC,
     resetActiveLevel = HIGH,
     softResetActiveLevel = HIGH,
-    clockEnableActiveLevel = HIGH)
+    clockEnableActiveLevel = HIGH
+  )
 
   // our devices
 
@@ -33,10 +34,12 @@ package object xilinx {
 
   val vu9p = XilinxDevice(UltraScale, "xcvu9p-flga2104-2-i", 800 MHz)
   //      val zybo = XilinxDevice(Series7, "xc7z010", 125 MHz, constraint = FileUtils.readFileToString(new File("./src/main/resources/zybo.xdc")))
-  val zcu104 = XilinxDevice(Series7, "xczu7ev-ffvc1156-2-e", 200 MHz)
+  val zcu104 = XilinxDevice(UltraScale, "xczu7ev-ffvc1156-2-e", 200 MHz)
   // TODO: find out part name for U250
-  val u250 = XilinxDevice(UltraScale, "XCU250-FIGD2104-2L-E".toLowerCase, 800 MHz)
-  val u200 = XilinxDevice(UltraScale, "XCU200-FSGD2104-2-E".toLowerCase, 800 MHz)
+  val u250 =
+    XilinxDevice(UltraScale, "XCU250-FIGD2104-2L-E".toLowerCase, 800 MHz)
+  val u200 =
+    XilinxDevice(UltraScale, "XCU200-FSGD2104-2-E".toLowerCase, 800 MHz)
   val kcu1500 = XilinxDevice(UltraScale, "xcku115-flvb2104-2-e", 800 MHz)
 
   val defaultDevice = u200
